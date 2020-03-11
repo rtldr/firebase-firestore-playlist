acf82b492b1ab97bb349c00fe90a52aa8fae2112
Sachin Bhargava <sachin20@uw.edu>
2020-03-11 05:16:23 -0700

    updated readme

---
+++ firebaseConfigProp.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA5lEm7EhDd9hleegzuVj4CcV8geM1TCvM",
+    authDomain: "firestore-tut-6fdf7.firebaseapp.com",
+    databaseURL: "https://firestore-tut-6fdf7.firebaseio.com",
+    projectId: "firestore-tut-6fdf7",
+    storageBucket: "firestore-tut-6fdf7.appspot.com",
+    messagingSenderId: "650647815715",
+    appId: "1:650647815715:web:51919e4fa12c310a081396"
+};
