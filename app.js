7fc26a4497f522451b3eb51e672b695f931ccb34
Sachin Bhargava <sachin20@uw.edu>
2020-03-11 05:13:15 -0700

    Finished till tut 4: setup, get, put

---
+++ app.js
@@ -0,0 +1,40 @@
+const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector("#add-cafe-form");
+
+// GET
+db.collection('cafes').get().then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+        render(doc);
+    });
+});
+
+// render a database doc to UI
+function render(doc) {
+    let name = document.createElement('span');
+    name.textContent = doc.data().name;
+
+    let city = document.createElement('span');
+    city.textContent = doc.data().city;
+
+    let li = document.createElement('li');
+    li.setAttribute('data-id', doc.id);
+    li.appendChild(name);
+    li.appendChild(city);
+
+    cafeList.appendChild(li);
+}
+
+// PUT
+form.addEventListener('submit', (e) => {
+    e.preventDefault();  // prevents the page from refreshing
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    }).then(doc => {
+        console.log(`wrote data with id ${doc.id}`);
+    }).catch(err => {
+        console.error('Error adding document: ', error);
+    });
+    form.name.value = '';
+    form.city.value = '';
+});
